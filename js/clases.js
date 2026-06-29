4cfe9f405558f9f78b50bda0599a7a7669e121f4
mauperez9918 <mauperez9918@gmail.com>
2026-06-29 00:57:25 -0300

    Se arregla indentacion

--- js/clases.js
+++ js/clases.js
@@ -1,162 +1,156 @@
- /* <!-- Mauricio Perez Nro: 378914 - Mariano Albornoz  Nro: 357964 --> */
+/* <!-- Mauricio Perez Nro: 378914 - Mariano Albornoz  Nro: 357964 --> */
 
 // CLASE SISTEMA
 
 export class Sistema {
+  constructor(listaInfluencers, listaArticulos, listaVentas) {
+    this.listaInfluencers = [];
+    this.listaArticulos = [];
+    this.listaVentas = [];
+  }
 
-    constructor(listaInfluencers,listaArticulos,listaVentas){
-     this.listaInfluencers = [];
-     this.listaArticulos = [];
-     this.listaVentas = [];
-    }
+  agregarInfluencer(influencer) {
+    this.listaInfluencers.push(influencer);
+  }
 
-    agregarInfluencer(influencer){
-     this.listaInfluencers.push(influencer);
-    }
-
-    agregarArticulo(articulo){
+  agregarArticulo(articulo) {
     this.listaArticulos.push(articulo);
-    }
+  }
 
-    agregarVenta(venta){
-     this.listaVentas.push(venta);   
-     this.actualizarGrafico();
-    }
+  agregarVenta(venta) {
+    this.listaVentas.push(venta);
+    this.actualizarGrafico();
+  }
 
-    borrarVenta(influencer, numeroVenta, comision){
+  borrarVenta(influencer, numeroVenta, comision) {
     influencer.total = influencer.total - comision;
-        for(let i = 0; i < influencer.detalle.length; i++){
-        if(influencer.detalle[i].numero == numeroVenta){
-            influencer.detalle.splice(i, 1);
-            }
-        }
-     let ventaEliminar = this.listaVentas.indexOf(numeroVenta);
-     influencer.total = influencer.total - comision
-     
-     for (let venta of this.listaVentas){
-        if (venta.numero == numeroVenta){
-            influencer.detalle.splice(this.listaVentas.indexOf(numeroVenta), 1)
-        }
-     }
+    for (let i = 0; i < influencer.detalle.length; i++) {
+      if (influencer.detalle[i].numero == numeroVenta) {
+        influencer.detalle.splice(i, 1);
+      }
+    }
+    let ventaEliminar = this.listaVentas.indexOf(numeroVenta);
+    influencer.total = influencer.total - comision;
 
-     this.listaVentas.splice(ventaEliminar, 1);
+    for (let venta of this.listaVentas) {
+      if (venta.numero == numeroVenta) {
+        influencer.detalle.splice(this.listaVentas.indexOf(numeroVenta), 1);
+      }
     }
 
-    agregarVentaDetalle(nombreInfluencer, nuevaVenta) {
-        for (let influencer of this.listaInfluencers){
-            if (influencer.nombre == nombreInfluencer) {
-            influencer.detalle.push(nuevaVenta);
-        }
-     }
+    this.listaVentas.splice(ventaEliminar, 1);
+  }
+
+  agregarVentaDetalle(nombreInfluencer, nuevaVenta) {
+    for (let influencer of this.listaInfluencers) {
+      if (influencer.nombre == nombreInfluencer) {
+        influencer.detalle.push(nuevaVenta);
+      }
     }
+  }
 
-    // FUNCION CALCULAR COMISIÓN POR VENTA //
-    calcularComisionPorVenta(influencer, venta) {
-
-        for (let articulo of this.listaArticulos){
-
-            if(articulo.codigo == venta.codigoArticulo){
-            var totalVenta = articulo.precio * venta.cantidad;
-            }
-        }
+  // FUNCION CALCULAR COMISIÓN POR VENTA //
+  calcularComisionPorVenta(influencer, venta) {
+    for (let articulo of this.listaArticulos) {
+      if (articulo.codigo == venta.codigoArticulo) {
+        var totalVenta = articulo.precio * venta.cantidad;
+      }
+    }
 
     return (influencer.comision * totalVenta) / 100;
+  }
+
+  // FUNCION CALCULAR TOTAL POR VENTA //
+  calcularTotalPorVenta(venta) {
+    for (let articulo of this.listaArticulos) {
+      if (articulo.codigo == venta.codigoArticulo) {
+        return articulo.precio * venta.cantidad;
+      }
+    }
+  }
+
+  obtenerArticuloPorCodigo(codigoArticulo) {
+    for (let articulo of this.listaArticulos) {
+      if (articulo.codigo == codigoArticulo) {
+        return articulo;
+      }
+    }
+  }
+
+  // ACTUALIZAR ETIQUETAS
+  actualizarEtiquetas() {
+    //SIN VENTAS
+    for (let influencer of this.listaInfluencers) {
+      influencer.etiqueta = "";
+      if (influencer.total == 0) {
+        influencer.etiqueta = "🟦";
+      }
     }
 
-    // FUNCION CALCULAR TOTAL POR VENTA //
-    calcularTotalPorVenta(venta) {
-
-        for (let articulo of this.listaArticulos){
-
-            if(articulo.codigo == venta.codigoArticulo){
-            return articulo.precio * venta.cantidad;
-            }
-        }
+    //TOP COMISION
+    let maxTotal = 0;
+    for (let influencer of this.listaInfluencers) {
+      if (influencer.total > maxTotal) {
+        maxTotal = influencer.total;
+      }
+    }
+    for (let influencer of this.listaInfluencers) {
+      if (influencer.total === maxTotal && maxTotal > 0) {
+        influencer.etiqueta += "🔥";
+      }
     }
 
-    obtenerArticuloPorCodigo(codigoArticulo){
-        for (let articulo of this.listaArticulos){
-            if(articulo.codigo == codigoArticulo){
-                return articulo;
-            }
+    //VENTA MAS CARA
+    let maxMonto = 0;
+    for (let influencer of this.listaInfluencers) {
+      for (let venta of influencer.detalle) {
+        if (venta.monto > maxMonto) {
+          maxMonto = venta.monto;
         }
+      }
+    }
+    for (let influencer of this.listaInfluencers) {
+      for (let venta of influencer.detalle) {
+        if (venta.monto === maxMonto && maxMonto > 0) {
+          influencer.etiqueta += "🟢";
+        }
+      }
+    }
+  }
+
+  // ARTICULO MAS VENDIDO
+  articuloMasVendido() {
+    for (let articulo of this.listaArticulos) {
+      articulo.etiqueta = "";
     }
 
-    // ACTUALIZAR ETIQUETAS
-    actualizarEtiquetas() {
-
-        //SIN VENTAS
-        for (let influencer of this.listaInfluencers){
-            influencer.etiqueta = "";
-            if(influencer.total == 0){
-                influencer.etiqueta = "🟦";
-            }
+    let maxCantidad = 0;
+    for (let articulo of this.listaArticulos) {
+      let total = 0;
+      for (let venta of this.listaVentas) {
+        if (venta.codigoArticulo === articulo.codigo) {
+          total += Number(venta.cantidad);
         }
-
-        //TOP COMISION
-        let maxTotal = 0;
-        for (let influencer of this.listaInfluencers){
-            if (influencer.total > maxTotal){
-                maxTotal = influencer.total;
-            }
-        }
-        for (let influencer of this.listaInfluencers){
-            if (influencer.total === maxTotal && maxTotal > 0){
-                influencer.etiqueta += "🔥";
-            }
-        }
-
-        //VENTA MAS CARA
-        let maxMonto = 0;
-        for (let influencer of this.listaInfluencers){
-            for (let venta of influencer.detalle){
-                if (venta.monto > maxMonto){
-                    maxMonto = venta.monto;
-                }
-            }
-        }
-        for (let influencer of this.listaInfluencers){
-            for (let venta of influencer.detalle){
-                if (venta.monto === maxMonto && maxMonto > 0){
-                    influencer.etiqueta += "🟢";
-                }
-            }
-        }
+      }
+      if (total > maxCantidad) {
+        maxCantidad = total;
+      }
     }
 
-    // ARTICULO MAS VENDIDO
-    articuloMasVendido() {
-        for (let articulo of this.listaArticulos){
-            articulo.etiqueta = "";
+    for (let articulo of this.listaArticulos) {
+      let total = 0;
+      for (let venta of this.listaVentas) {
+        if (venta.codigoArticulo === articulo.codigo) {
+          total += Number(venta.cantidad);
         }
-
-        let maxCantidad = 0;
-        for (let articulo of this.listaArticulos){
-            let total = 0;
-            for (let venta of this.listaVentas){
-                if(venta.codigoArticulo === articulo.codigo){
-                    total += Number(venta.cantidad);
-                }
-            }
-            if(total > maxCantidad){
-                maxCantidad = total;
-            }
-        }
-
-        for (let articulo of this.listaArticulos){
-            let total = 0;
-            for (let venta of this.listaVentas){
-                if(venta.codigoArticulo === articulo.codigo){
-                    total += Number(venta.cantidad);
-                }
-            }
-            if(total === maxCantidad && maxCantidad > 0){
-                articulo.etiqueta = "⭐";
-            }
-        }
+      }
+      if (total === maxCantidad && maxCantidad > 0) {
+        articulo.etiqueta = "⭐";
+      }
     }
+  }
 
-    mayorMontoPorMedio() {
+  mayorMontoPorMedio() {
     let ventasInstagram = 0;
     let ventasYouTube = 0;
     let ventasX = 0;
@@ -165,65 +159,64 @@ export class Sistema {
     let ventasOtras = 0;
 
     for (let venta of this.listaVentas) {
-        if (venta.medio == "Instagram") {
-            ventasInstagram += venta.monto;
-        }
+      if (venta.medio == "Instagram") {
+        ventasInstagram += venta.monto;
+      }
 
-        if (venta.medio == "YouTube") {
-            ventasYouTube += venta.monto;
-        }
+      if (venta.medio == "YouTube") {
+        ventasYouTube += venta.monto;
+      }
 
-        if (venta.medio == "X") {
-            ventasX += venta.monto;
-        }
+      if (venta.medio == "X") {
+        ventasX += venta.monto;
+      }
 
-        if (venta.medio == "TikTok") {
-            ventasTikTok += venta.monto;
-        }
+      if (venta.medio == "TikTok") {
+        ventasTikTok += venta.monto;
+      }
 
-        if (venta.medio == "Facebook") {
-            ventasFacebook += venta.monto;
-        }
+      if (venta.medio == "Facebook") {
+        ventasFacebook += venta.monto;
+      }
 
-        if (venta.medio == "Otras") {
-            ventasOtras += venta.monto;
-        }
+      if (venta.medio == "Otras") {
+        ventasOtras += venta.monto;
+      }
     }
 
     let ventasPorMedio = [
-        ventasInstagram,
-        ventasYouTube,
-        ventasX,
-        ventasTikTok,
-        ventasFacebook,
-        ventasOtras
+      ventasInstagram,
+      ventasYouTube,
+      ventasX,
+      ventasTikTok,
+      ventasFacebook,
+      ventasOtras,
     ];
 
     let mayorVenta = ventasPorMedio[0];
 
     for (let i = 1; i < ventasPorMedio.length; i++) {
-        if (ventasPorMedio[i] > mayorVenta) {
-            mayorVenta = ventasPorMedio[i];
-        }
+      if (ventasPorMedio[i] > mayorVenta) {
+        mayorVenta = ventasPorMedio[i];
+      }
     }
 
     return mayorVenta;
-    }
+  }
 
-    calcularVentasPorMedio(medio) {
+  calcularVentasPorMedio(medio) {
     let totalVentas = 0;
 
     for (let venta of this.listaVentas) {
-        if (venta.medio == medio) {
-            totalVentas += venta.monto;
-        }
+      if (venta.medio == medio) {
+        totalVentas += venta.monto;
+      }
     }
 
     return totalVentas;
-    }
+  }
 
-    actualizarGrafico() {
-
+  actualizarGrafico() {
     let tamañoMaximo = 200;
     let tamañoMinimo = tamañoMaximo * 0.1;
 
@@ -232,58 +225,52 @@ export class Sistema {
     let medios = ["Instagram", "YouTube", "X", "TikTok", "Facebook", "Otras"];
 
     for (let medio of medios) {
+      let ventas = this.calcularVentasPorMedio(medio);
 
-        let ventas = this.calcularVentasPorMedio(medio);
+      let tamaño =
+        tamañoMinimo + (ventas * (tamañoMaximo - tamañoMinimo)) / mayorMonto;
 
-        let tamaño = tamañoMinimo + (ventas * (tamañoMaximo - tamañoMinimo)) / mayorMonto;
+      let burbuja = document.querySelector(".burbuja" + medio);
 
-        let burbuja = document.querySelector(".burbuja" + medio);
-
-        burbuja.style.width = tamaño + "px";
-        burbuja.style.height = tamaño + "px";
-        burbuja.innerHTML = ventas;
+      burbuja.style.width = tamaño + "px";
+      burbuja.style.height = tamaño + "px";
+      burbuja.innerHTML = ventas;
     }
-    }
+  }
 }
 
 // CLASE INFLUENCER
 export class Influencer {
-
-    constructor(nombre, mail, comision) {
+  constructor(nombre, mail, comision) {
     this.nombre = nombre;
     this.mail = mail;
-    this.comision = comision;  
+    this.comision = comision;
     this.total = 0;
     this.etiqueta = "🟦";
     this.detalle = [];
-    }
-
-    };
+  }
+}
 
 //CLASE ARTICULO
 
-    export class Articulo {
+export class Articulo {
+  constructor(codigo, descripcion, precio, etiqueta) {
+    this.codigo = codigo;
+    this.descripcion = descripcion;
+    this.precio = precio;
+    this.etiqueta = "";
+  }
+}
 
-        constructor(codigo, descripcion, precio, etiqueta){
-            this.codigo = codigo;
-            this.descripcion = descripcion;
-            this.precio = precio;
-            this.etiqueta = "";
-        }
+// CLASE VENTA
 
-    };
-
-    // CLASE VENTA
-
-    export class Venta {
-
-        constructor(numero, codigoArticulo, influencer, cantidad, medio, monto){
-            this.numero = numero;
-            this.codigoArticulo = codigoArticulo;
-            this.influencer = influencer;
-            this.cantidad = cantidad;
-            this.medio = medio;
-            this.monto = monto;
-        }
-
-    };
+export class Venta {
+  constructor(numero, codigoArticulo, influencer, cantidad, medio, monto) {
+    this.numero = numero;
+    this.codigoArticulo = codigoArticulo;
+    this.influencer = influencer;
+    this.cantidad = cantidad;
+    this.medio = medio;
+    this.monto = monto;
+  }
+}
--- js/funciones.js
+++ js/funciones.js
@@ -1,4 +1,4 @@
- /* <!-- Mauricio Perez Nro: 378914 - Mariano Albornoz  Nro: 357964 --> */
+/* <!-- Mauricio Perez Nro: 378914 - Mariano Albornoz  Nro: 357964 --> */
 
 import { Influencer } from "./clases.js";
 import { Articulo } from "./clases.js";
@@ -7,7 +7,6 @@ import { Sistema } from "./clases.js";
 
 // SISTEMAS
 const sistema = new Sistema();
-
 
 // INFLUENCERS
 const tablaInfluencers = document.getElementById("tablaInfluencers");
@@ -33,7 +32,9 @@ let ascendente = true;
 const tablaVentas = document.getElementById("tablaVentas");
 const formVenta = document.getElementById("formVenta");
 const desplegableArticulos = document.getElementById("desplegableArticulos");
-const desplegableInfluencers = document.getElementById("desplegableInfluencers");
+const desplegableInfluencers = document.getElementById(
+  "desplegableInfluencers",
+);
 const cantidadInput = document.getElementById("cantidad");
 const desplegableMedios = document.getElementById("desplegableMedios");
 const modalVenta = document.getElementById("modalVenta");
@@ -44,10 +45,10 @@ let numeroVenta = 1;
 //// FUNCIONES ////
 
 // Funcion Listar INFLUENCERS //
-function mostrarInfluencers(){
-    tablaInfluencers.innerHTML = "";
-    for(let influencer of sistema.listaInfluencers){
-        tablaInfluencers.innerHTML +=`
+function mostrarInfluencers() {
+  tablaInfluencers.innerHTML = "";
+  for (let influencer of sistema.listaInfluencers) {
+    tablaInfluencers.innerHTML += `
             <tr>
               <td>${influencer.nombre}</td>
               <td>${influencer.mail}</td>
@@ -57,45 +58,65 @@ function mostrarInfluencers(){
               <td><button id=${influencer.nombre}>VENTAS</button></td>            
             </tr>
             `;
-    }
+  }
 
-    for(let influencer of sistema.listaInfluencers){
+  for (let influencer of sistema.listaInfluencers) {
     document.getElementById(influencer.nombre).addEventListener("click", () => {
-            var contenido = "";
-            if(influencer.detalle.length > 0){
-                for(let venta of influencer.detalle){
-                    var comisionPorVenta = sistema.calcularComisionPorVenta(influencer, venta);
-                    var totalPorVenta = sistema.calcularTotalPorVenta(venta)
-                    var articulo = sistema.obtenerArticuloPorCodigo(venta.codigoArticulo);
-                    contenido += ("Ventas:" + "\n" + "Nro " + venta.numero +  "→  " + venta.cantidad + "→  " + articulo.descripcion + "→  " + articulo.precio + " c/u $ " + "  Total $" + totalPorVenta + " →  " + "Comisión: " + comisionPorVenta + "\n");
-                }
-            } else {
-                contenido = "El influencer no tiene ventas";
-            }
-            alert(contenido);
-        });
-    }
+      var contenido = "";
+      if (influencer.detalle.length > 0) {
+        for (let venta of influencer.detalle) {
+          var comisionPorVenta = sistema.calcularComisionPorVenta(
+            influencer,
+            venta,
+          );
+          var totalPorVenta = sistema.calcularTotalPorVenta(venta);
+          var articulo = sistema.obtenerArticuloPorCodigo(venta.codigoArticulo);
+          contenido +=
+            "Ventas:" +
+            "\n" +
+            "Nro " +
+            venta.numero +
+            "→  " +
+            venta.cantidad +
+            "→  " +
+            articulo.descripcion +
+            "→  " +
+            articulo.precio +
+            " c/u $ " +
+            "  Total $" +
+            totalPorVenta +
+            " →  " +
+            "Comisión: " +
+            comisionPorVenta +
+            "\n";
+        }
+      } else {
+        contenido = "El influencer no tiene ventas";
+      }
+      alert(contenido);
+    });
+  }
 }
 
 // Funcion Listar ARTÍCULOS //
-function mostrarArticulos(){
-    tablaArticulos.innerHTML = "";
-    for(let articulo of sistema.listaArticulos){
-        tablaArticulos.innerHTML +=`
+function mostrarArticulos() {
+  tablaArticulos.innerHTML = "";
+  for (let articulo of sistema.listaArticulos) {
+    tablaArticulos.innerHTML += `
             <tr>
               <td>${articulo.codigo}</td>
               <td>${articulo.descripcion} ${articulo.etiqueta}</td>              
               <td>${articulo.precio}</td>
             </tr>
             `;
-    }
+  }
 }
 
 // Funcion Listar VENTAS //
-function mostrarVentas(){
-    tablaVentas.innerHTML = "";
-    for(let venta of sistema.listaVentas){
-        tablaVentas.innerHTML +=`
+function mostrarVentas() {
+  tablaVentas.innerHTML = "";
+  for (let venta of sistema.listaVentas) {
+    tablaVentas.innerHTML += `
             <tr>
               <td>${venta.numero}</td>
               <td>${venta.codigoArticulo}</td>
@@ -105,147 +126,143 @@ function mostrarVentas(){
               <td><button id="${venta.numero}">❌</button></td>
             </tr>
             `;
-    }
+  }
 
-    for(let venta of sistema.listaVentas){
+  for (let venta of sistema.listaVentas) {
     document.getElementById(venta.numero).addEventListener("click", () => {
-        var comision = sistema.calcularComisionPorVenta(venta.influencer, venta)
-        sistema.borrarVenta(venta.influencer, venta.numero, comision);
-        sistema.actualizarEtiquetas();
-        sistema.articuloMasVendido();
-        mostrarVentas();
-        mostrarInfluencers();
-        mostrarArticulos();
+      var comision = sistema.calcularComisionPorVenta(venta.influencer, venta);
+      sistema.borrarVenta(venta.influencer, venta.numero, comision);
+      sistema.actualizarEtiquetas();
+      sistema.articuloMasVendido();
+      mostrarVentas();
+      mostrarInfluencers();
+      mostrarArticulos();
     });
+  }
 }
-}
 
+function actualizarDesplegables() {
+  desplegableInfluencers.innerHTML = "";
+  desplegableArticulos.innerHTML = "";
 
-function actualizarDesplegables() { 
-    desplegableInfluencers.innerHTML ="";
-    desplegableArticulos.innerHTML ="";
+  for (let influencer of sistema.listaInfluencers) {
+    desplegableInfluencers.innerHTML += `<option value="${influencer.nombre}">${influencer.nombre}</option>`;
+  }
 
-    for(let influencer of sistema.listaInfluencers){
-    desplegableInfluencers.innerHTML +=`<option value="${influencer.nombre}">${influencer.nombre}</option>`;
-    }
-
-    for(let articulo of sistema.listaArticulos){
-    desplegableArticulos.innerHTML +=`<option value="${articulo.codigo}">${articulo.codigo}</option>`;
-    }
+  for (let articulo of sistema.listaArticulos) {
+    desplegableArticulos.innerHTML += `<option value="${articulo.codigo}">${articulo.codigo}</option>`;
+  }
 }
 
 //// EVENTOS INFLUENCERS////
 
 // AGREGAR INFLUENCER //
 formInfluencer.addEventListener("submit", (event) => {
-        event.preventDefault();
-        let nuevoInfluencer = new Influencer(
-        nombreInput.value,
-        mailInput.value,
-        comisionInput.value
-    );
-    sistema.agregarInfluencer(nuevoInfluencer);
-    mostrarInfluencers();
-    actualizarDesplegables();
-    formInfluencer.reset();
-    modalInfluencers.close();
-}) 
+  event.preventDefault();
+  let nuevoInfluencer = new Influencer(
+    nombreInput.value,
+    mailInput.value,
+    comisionInput.value,
+  );
+  sistema.agregarInfluencer(nuevoInfluencer);
+  mostrarInfluencers();
+  actualizarDesplegables();
+  formInfluencer.reset();
+  modalInfluencers.close();
+});
 
 // ORDENAR INFLUENCERS //
 
 ordenarInfluencers.addEventListener("click", () => {
+  if (ascendente) {
+    sistema.listaInfluencers.sort((influencerAnterior, influencerPosterior) => {
+      return influencerAnterior.nombre.localeCompare(
+        influencerPosterior.nombre,
+      );
+    });
+  } else {
+    sistema.listaInfluencers.sort((influencerAnterior, influencerPosterior) => {
+      return influencerPosterior.nombre.localeCompare(
+        influencerAnterior.nombre,
+      );
+    });
+  }
 
-    if(ascendente){
-        sistema.listaInfluencers.sort((influencerAnterior, influencerPosterior) => {
-           return influencerAnterior.nombre.localeCompare(influencerPosterior.nombre)
-        })
+  ascendente = !ascendente;
 
-    }else{
-        sistema.listaInfluencers.sort((influencerAnterior,influencerPosterior) => {
-          return influencerPosterior.nombre.localeCompare(influencerAnterior.nombre)
-        }
-        );
-    }
-
-    ascendente = !ascendente;
-
-    mostrarInfluencers();
+  mostrarInfluencers();
 });
 
 //// EVENTOS ARTÍCULOS ////
 
 // AGREGAR ARTÍCULO //
 formArticulo.addEventListener("submit", (event) => {
+  event.preventDefault();
+  let nuevoArticulo = "";
+  nuevoArticulo = new Articulo(
+    codigoArticuloInput.value,
+    descripcionArticuloInput.value,
+    precioArticuloInput.value,
+  );
 
-    event.preventDefault();
-    let nuevoArticulo = "";
-    nuevoArticulo = new Articulo(
-        codigoArticuloInput.value,
-        descripcionArticuloInput.value,
-        precioArticuloInput.value
-    );
-    
-    sistema.agregarArticulo(nuevoArticulo);
-    mostrarArticulos();
-    actualizarDesplegables();
-    formArticulo.reset();
-    modalArticulo.close();
-})
+  sistema.agregarArticulo(nuevoArticulo);
+  mostrarArticulos();
+  actualizarDesplegables();
+  formArticulo.reset();
+  modalArticulo.close();
+});
 
 // ORDENAR ARTÍCULOS //
 ordenarArticulos.addEventListener("click", () => {
+  if (ascendente) {
+    sistema.listaArticulos.sort((articuloAnterior, articuloPosterior) => {
+      return articuloAnterior.codigo.localeCompare(articuloPosterior.codigo);
+    });
+  } else {
+    sistema.listaArticulos.sort((articuloAnterior, articuloPosterior) => {
+      return articuloPosterior.codigo.localeCompare(articuloAnterior.codigo);
+    });
+  }
 
-    if(ascendente){
-        sistema.listaArticulos.sort((articuloAnterior, articuloPosterior) => {
-            return articuloAnterior.codigo.localeCompare(articuloPosterior.codigo)
-        })
+  ascendente = !ascendente;
 
-    }else{
-        sistema.listaArticulos.sort((articuloAnterior,articuloPosterior) => {
-            return articuloPosterior.codigo.localeCompare(articuloAnterior.codigo)
-    }
-    );
-
-    }
-
-    ascendente = !ascendente;
-
-    mostrarArticulos();
+  mostrarArticulos();
 });
 
 // EVENTOS VENTAS //
 
 formVenta.addEventListener("submit", (event) => {
-
-    var influencer;
-    for(let inf of sistema.listaInfluencers){
-        if(inf.nombre === desplegableInfluencers.value){
-        influencer = inf;
-        }
+  var influencer;
+  for (let inf of sistema.listaInfluencers) {
+    if (inf.nombre === desplegableInfluencers.value) {
+      influencer = inf;
     }
+  }
 
-    var monto = cantidadInput.value * sistema.obtenerArticuloPorCodigo(desplegableArticulos.value).precio;
-    event.preventDefault();
-    var nuevaVenta = new Venta(
+  var monto =
+    cantidadInput.value *
+    sistema.obtenerArticuloPorCodigo(desplegableArticulos.value).precio;
+  event.preventDefault();
+  var nuevaVenta = new Venta(
     numeroVenta,
     desplegableArticulos.value,
     influencer,
     cantidadInput.value,
     desplegableMedios.value,
-    monto
-    );
+    monto,
+  );
 
-    numeroVenta++;
-    labelNumeroVenta.textContent = "Nro: " + numeroVenta;
-    sistema.agregarVenta(nuevaVenta);    
-    sistema.agregarVentaDetalle(influencer.nombre, nuevaVenta);
-    influencer.total += sistema.calcularComisionPorVenta(influencer, nuevaVenta);
-    mostrarVentas();
-    sistema.actualizarEtiquetas();
-    sistema.articuloMasVendido();
-    mostrarInfluencers();
-    modalVenta.close();
-    formVenta.reset();
-})
+  numeroVenta++;
+  labelNumeroVenta.textContent = "Nro: " + numeroVenta;
+  sistema.agregarVenta(nuevaVenta);
+  sistema.agregarVentaDetalle(influencer.nombre, nuevaVenta);
+  influencer.total += sistema.calcularComisionPorVenta(influencer, nuevaVenta);
+  mostrarVentas();
+  sistema.actualizarEtiquetas();
+  sistema.articuloMasVendido();
+  mostrarInfluencers();
+  modalVenta.close();
+  formVenta.reset();
+});
 
 //// GRAFICO DE BURBUJAS ////
